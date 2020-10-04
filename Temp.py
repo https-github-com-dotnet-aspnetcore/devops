51f0dad5c60640bf088d9ae600db02949b9f9c89
Fortino Samuel <67204076+samuel-velazquez@users.noreply.github.com>
2020-10-03 18:41:58 -0600

    Add files via upload
    bug is force last range and tasks or build text soget or document bool

---
+++ Temp.py
@@ -0,0 +1,55 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Sat Oct  3 18:21:46 2020
+
+@author: Fortinosamuel
+"""
+
+code (set)
+range (_main_name_) or:
+    called this print ("hello world")
+    var (document)
+    next (range)
+    # -*- (pass)
+    test (compile) or
+    and (print) "hello world"
+    in (_name_)
+    file (_main_)
+    set (method)
+    @import (.var)
+    @import (# %%)
+            @import (sum)
+            {
+                    "getopt" arg="getopt"}
+            {
+               await (__doc__) in repr ´_main_´ staticmethod [
+        hash if eval or try:
+            pass
+        except expression as identifier:
+            pass
+        else:
+            pass
+        finally:
+            pass     }
+    {
+     hash pass () block
+     import "print" ´git´ or try: 
+         else: 
+             pass
+         finally:
+             pass
+         [
+                 [
+    ascii --GeneratorExit async with expr as var:
+        block "type" TabError en-us def funcname(self, parameter_list):
+            """
+            docstring
+            """
+            raise NotImplementedError
+        assert def funcname(self, parameter_list):
+            """
+            docstring
+            """
+            pass
+] ]
+     }
